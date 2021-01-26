49903285f774847aad70f6f0ebbeaa4d0f15b266
cyan <cyan>
2021-01-26 17:18:04 +0800

    update

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -8,11 +8,11 @@ const banner = `
   / __ \/ __/ __/ __ \|   /
  / / / / /_/ /_/ /_/ /   |
 /_/ /_/\__/\__/ .___/_/|_|
-             /_/              v1.0.3
+             /_/              v1.0.3.1
 `
 
 // Version is the current version of httpx
-const Version = `1.0.3`
+const Version = `1.0.3.1`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
